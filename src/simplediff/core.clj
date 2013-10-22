641445ab80a34ccd8db1d719d5a36e079841ea25
Joshua Calloway <joshua@josh-asus.(none)>
2013-10-21 22:24:08 -0500

    added comments

--- src/simplediff/core.clj
+++ src/simplediff/core.clj
@@ -2,6 +2,10 @@
 
 (use 'simplediff.diff)
 
+(defrecord FileDiff [fileA fileB])
+
+(defn make [a b] (FileDiff. a b))
+
 (defn -main [& args]
   (if (= 2 (count args))
     (doseq [i (dodiff (make (first args) (second args)))]
--- src/simplediff/diff.clj
+++ src/simplediff/diff.clj
@@ -1,32 +1,35 @@
 (ns simplediff.diff
   (:use clojure.java.io))
 
-(defrecord FileDiff [fileA fileB])
-
-(defn make [a b] (FileDiff. a b))
-
+;; Tests whether line exits in lines list
+;; > (exists-line? "first line" ["other lines" "some lines"])
+;; > false
+;;
 (defn exists-line? [line lines]
   (if (empty? lines)
     false
     (if (= line (first lines)) true
         (recur line (rest lines)))))
 
-
+;; Do compare for all lines in linesA with all lines in linesB.
+;; cur-line-noA and cur-line-noB serve as line number references
+;; for generating the specific line numbers in the difference return value
 (defn docompare[linesA cur-line-noA linesB cur-line-noB]
   (let [cur-line-noA++ (inc cur-line-noA)
         cur-line-noB++ (inc cur-line-noB)
         carA (first linesA)
-        carB (first linesB)
+        carB (first linesB)  ; car refers to first element of list
         cdrA (rest linesA)
-        cdrB (rest linesB)
+        cdrB (rest linesB)   ; cdr refers to list without first element 
         str> #(str "> " %)
         str< #(str "< " %)
-        adiff #(list* %1 "a" %2 [(str> %3)] )
-        ddiff #(list* %1 "d" %2 [(str< %3)] )
-        cdiff #(list* %1 "c" %2 (str< %3) [(str> %4)] )         
+        adiff #(list* %1 "a" %2 [(str> %3)] ) ; Add difference
+        ddiff #(list* %1 "d" %2 [(str< %3)] ) ; Delete difference
+        cdiff #(list* %1 "c" %2 (str< %3) [(str> %4)] ) ; Change difference     
         ]
     (cond
      (and (empty? linesA) (empty? linesB)) nil
+     
      (empty? linesA) (cons (adiff cur-line-noA cur-line-noB++ carB)
                            (docompare linesA cur-line-noA cdrB cur-line-noB++))
 
@@ -34,13 +37,16 @@
                            (docompare cdrA cur-line-noA++ linesB cur-line-noB))
 
      (= carA carB) (docompare cdrA cur-line-noA++ cdrB cur-line-noB++)
+     
      (exists-line? carB cdrA) (cons (ddiff cur-line-noA++ cur-line-noB carA) (docompare cdrA cur-line-noA++ linesB cur-line-noB))
+     
      (exists-line? carA cdrB) (cons (adiff cur-line-noA cur-line-noB++ carB) (docompare linesA cur-line-noA cdrB cur-line-noB++))
+     
      :else (cons (cdiff cur-line-noA++ cur-line-noB++ carA carB) (docompare cdrA cur-line-noA++ cdrB cur-line-noB++))
      ))
   )
 
-
+;; Top-level method of namespace.  D is a Diff object
 (defn dodiff [D]
   (let [rdrA (reader (:fileA D))
         rdrB (reader (:fileB D))
